8eab0ce42872455b8ca814bc466c5290d655f0ae
zhezzma <250994229@qq.com>
2024-10-20 15:53:13 +0800

    update sh

--- scripts/run_daily.sh
+++ scripts/run_daily.sh
@@ -11,21 +11,26 @@ source /root/.nvm/nvm.sh
 # Change directory to the application directory
 cd /root/workspace/microsoft-rewards-script
 
-# Define the minimum and maximum wait times in seconds
-MINWAIT=$((5*60))  # 5 minutes
-MAXWAIT=$((50*60)) # 50 minutes
+# Check if NO_SLEEP is set and true
+if [[ -z "${NO_SLEEP}" || "${NO_SLEEP}" != "true" ]]; then
+    # Define the minimum and maximum wait times in seconds
+    MINWAIT=$((5*60))  # 5 minutes
+    MAXWAIT=$((50*60)) # 50 minutes
 
-# Calculate a random sleep time within the specified range
-SLEEPTIME=$((MINWAIT + RANDOM % (MAXWAIT - MINWAIT)))
+    # Calculate a random sleep time within the specified range
+    SLEEPTIME=$((MINWAIT + RANDOM % (MAXWAIT - MINWAIT)))
 
-# Convert the sleep time to minutes for logging
-SLEEP_MINUTES=$((SLEEPTIME / 60))
+    # Convert the sleep time to minutes for logging
+    SLEEP_MINUTES=$((SLEEPTIME / 60))
 
-# Log the sleep duration
-echo "Sleeping for $SLEEP_MINUTES minutes ($SLEEPTIME seconds)..."
+    # Log the sleep duration
+    echo "Sleeping for $SLEEP_MINUTES minutes ($SLEEPTIME seconds)..."
 
-# Sleep for the calculated time
-sleep $SLEEPTIME
+    # Sleep for the calculated time
+    sleep $SLEEPTIME
+else
+    echo "NO_SLEEP is set to true. Skipping sleep..."
+fi
 
 # Log the start of the script
 echo "Starting script..."
@@ -34,4 +39,4 @@ echo "Starting script..."
 node src/updateConfig.js
 
 # Execute the Node.js script directly
-npm run start
+xvfb-run --server-args="-screen 0 1280x1024x24" npm run start
